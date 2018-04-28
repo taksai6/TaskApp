c1dd39d4269a66e66f975eee87fd2676dc5e266b
taksai6 <ta93_5@yahoo.co.jp>
2018-04-28 16:12:24 +0900

    Task

---
+++ app/src/main/java/jp/techacademy/takumi/nakamura/taskapp/MainActivity.java
@@ -0,0 +1,180 @@
+package jp.techacademy.takumi.nakamura.taskapp;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
+import io.realm.Sort;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_TASK = "jp.techacademy.takumi.nakamura.taskapp.TASK";
+
+    private Realm mRealm;
+    private RealmResults<Task> mTaskRealmResults;
+
+
+    private RealmChangeListener mRealmListener = new RealmChangeListener() {
+
+
+        @Override
+        public void onChange(Object element) {
+            reloadListView();
+        }
+    };
+    private ListView mListView;
+    private TaskAdapter mTaskAdapter;
+    private EditText mSearchEdit;
+    private Button mDecideButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        mDecideButton = (Button) findViewById(R.id.decide_button);
+        mSearchEdit = (EditText) findViewById(R.id.search_edit_text);
+
+        // Realmの設定
+        mRealm = Realm.getDefaultInstance();
+        mRealm.addChangeListener(mRealmListener);
+
+        // ListViewの設定
+        mTaskAdapter = new TaskAdapter(MainActivity.this);
+        mListView = (ListView) findViewById(R.id.listView1);
+
+
+        mDecideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search();
+            }
+        });
+
+        // ListViewをタップしたときの処理
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 入力・編集する画面に遷移させる
+                Task task = (Task) parent.getAdapter().getItem(position);
+
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                intent.putExtra(EXTRA_TASK, task.getId());
+
+                startActivity(intent);
+            }
+        });
+
+        // ListViewを長押ししたときの処理
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // タスクを削除する
+
+                final Task task = (Task) parent.getAdapter().getItem(position);
+
+                // ダイアログを表示する
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle("削除");
+                builder.setMessage(task.getTitle() + "を削除しますか");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RealmResults<Task> results = mRealm.where(Task.class).equalTo("id", task.getId()).findAll();
+
+                        mRealm.beginTransaction();
+                        results.deleteAllFromRealm();
+                        mRealm.commitTransaction();
+
+                        Intent resultIntent = new Intent(getApplicationContext(), TaskAlarmReceiver.class);
+                        PendingIntent resultPendingIntent = PendingIntent.getBroadcast(
+                                MainActivity.this,
+                                task.getId(),
+                                resultIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+
+                        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                        alarmManager.cancel(resultPendingIntent);
+
+
+                        reloadListView();
+                    }
+                });
+                builder.setNegativeButton("CANCEL", null);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                return true;
+            }
+        });
+
+       all();
+    }
+
+
+    private void search() {
+        String searchquery = mSearchEdit.getText().toString();
+        RealmResults<Task> result2 = mRealm.where(Task.class).equalTo("category", searchquery).findAll();
+        mTaskRealmResults = result2;
+
+        reloadListView();
+    }
+
+    private void all() {
+        // Realmデータベースから、「全てのデータを取得して新しい日時順に並べた結果」を取得
+        RealmResults<Task> result1 = mRealm.where(Task.class).findAllSorted("date", Sort.DESCENDING);
+        mTaskRealmResults = result1;
+        reloadListView();
+    }
+
+
+    private void reloadListView() {
+        // 上記の結果を、TaskList としてセットする
+        mTaskAdapter.setTaskList(mRealm.copyFromRealm(mTaskRealmResults));
+        // TaskのListView用のアダプタに渡す
+        mListView.setAdapter(mTaskAdapter);
+        // 表示を更新するために、アダプターにデータが変更されたことを知らせる
+        mTaskAdapter.notifyDataSetChanged();
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mRealm.close();
+    }
+
+
+}
+
